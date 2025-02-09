a22dbf7fd0eaa34083de9b4cdbb8d71daa84db0b
Ashutoshjaiswal975 <ashutosh.jaiswal.a901@gmail.com>
2025-02-09 16:21:25 +0530

    just adding logoutBtn

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -2,6 +2,7 @@ import Header from "./header/Header";
 import Footer from "./footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
+import LogoutBtn from "./header/LogoutBtn";
 
 // this file indication fo direct and short tricks to just importing and 
 // exporting the header footer in short method just to import index.js
@@ -12,5 +13,6 @@ export  {
     Header, 
     Footer,
     Logo,
-    Container
+    Container,
+    LogoutBtn,
 }
